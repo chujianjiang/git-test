752e6e6ab80291396b2c48d16b814e085bd33f58
chujianjiang <108724980+chujianjiang@users.noreply.github.com>
2022-07-07 17:39:36 +0800

    pull commit

--- src/main/java/chujian/git/gGtTest.java
+++ src/main/java/chujian/git/gGtTest.java
@@ -10,6 +10,7 @@ public class gGtTest {
         System.out.println("master test!!");
         System.out.println("hot-fix test!!");
         System.out.println("push test!!");
+        System.out.println("pull test!!");
     }
 
 }
